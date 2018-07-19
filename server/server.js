e920d792b51b81deea115af1143e2b4f0263ceea
edo9k <edox86@gmail.com>
2018-07-19 14:11:20 -0300

    More Mongoose Stuff
    Aula 71, usando Validator, Type e Default do Mongoose (Node/Mongo ORM)

--- server/server.js
+++ server/server.js
@@ -5,22 +5,53 @@ mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 let Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 })
 
-let newTodo = new Todo({
-  text: 'Cook dinner' 
+//let otherTodo = new Todo({
+//  text: ' Test 2  ',
+//  //completed: true,
+//  //completedAt: 123
+//})
+//
+//otherTodo.save().then((doc) => {
+//  console.log(JSON.stringify(doc, undefined, 2))
+//}, e => {
+//  console.log('Unable to save', e)
+//})
+
+// User 
+// email - require it - trim it - set type - set min length of 1
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 })
 
-newTodo.save().then(doc => {
-  console.log('Saved todo', doc)
-}, e => {
-  console.log('Unable to save todo')
+
+let user = new User({
+  email: 'user1@example.com  '
 })
+
+user.save().then(doc => {
+  console.log('User saved', doc)
+}, e => {
+  console.log('Unable to save user', e)
+})
+
+
