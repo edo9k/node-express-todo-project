01702d3a2c56cb4d7ae492fbae6353c6cda0e001
edo9k <edox86@gmail.com>
2018-08-01 14:38:59 -0300

    Add Get /todos/:id (Lesson 78)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-let {mongoose} = require('./db/mongoose')
-let {Todo} = require('./models/todo')
-let {User} = require('./models/user')
+const {ObjectID} = require('mongodb')
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
 
 let app = express()
 
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
   }, e => {
     res.status(400).send(e)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+   let id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})   
+  }).catch(e => {
+    res.status(400).send()
+  })
+
 })
 
 app.listen(3000, () => {
