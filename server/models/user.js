102de20602b5a54b27bfe9d8ce5e7fb91c09f33a
edo9k <edox86@gmail.com>
2018-08-09 16:30:28 -0300

    Add GET /users/me and auth middleware (L91)
    Yeah, pretty much that. Completely unsecure though. Gotta work on that,
    specially learning how to use Helmet, TSL and other secure techniques.
    
    Signed-off-by: edo9k <edox86@gmail.com>

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,24 @@ UserSchema.methods.generateAuthToken = function () {
   })
 }
 
+UserSchema.statics.findByToken = function (token) {
+  let User = this
+  let decoded 
+
+  try {
+    decoded = jwt.verify(token, 'this-password-policy-is-shit')
+  } catch (e) {
+    return Promise.reject()
+  }
+
+  return  User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+
+}
+
 let User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
