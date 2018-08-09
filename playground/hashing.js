f6cb5d3945d1c6a43e4bc191b27e7822ba735fa7
edo9k <edox86@gmail.com>
2018-08-09 17:26:31 -0300

    Store hashed password (L92)
    bcrypt trapdoor one way function strikes again
    
    Signed-off-by: edo9k <edox86@gmail.com>

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,35 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const data = {
-  id: 10
-}
+const password = 'somesecretpassphraseshit'
 
-const token = jwt.sign(data, 'weakafpassword')
-console.log(token)
+/*
+bcrypt.genSalt(10, (err, salt) => {
+  console.log(`salt: ${salt}`)
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(`password: ${password}\nhash: ${hash}\n`)
+  })
+})
+*/
 
-const decoded = jwt.verify(token, 'weakafpassword')
-console.log('decoded: ',decoded)
+const hashedPassword = '$2a$10$Bhx3Q.HU52LtrnXnxRgYt.D5iYg4EpuoKI3MaYNz/54VniZm0a0gi'
 
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
+
+//
+//const data = {
+//  id: 10
+//}
+//
+//const token = jwt.sign(data, 'weakafpassword')
+//console.log(token)
+//
+//const decoded = jwt.verify(token, 'weakafpassword')
+//console.log('decoded: ',decoded)
+//
 //const message = 'I am user number 3'
 //const hash = SHA256(message).toString()
 //
